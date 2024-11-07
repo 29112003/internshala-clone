fb5c92c5397d702be6281f068d7da9ab27056aab
29112003 <thisiskeshavyadavv@gmail.com>
2024-11-07 11:20:55 +0530

    setuping nodemailer

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,7 @@
 const { catchAsyncErrors } = require("../middlewares/catchAsyncErrors");
 const Student = require("../models/studentModel");
 const ErrorHandler = require("../utils/errorHandler");
+const { sendmail } = require("../utils/nodemailer");
 const { sendtoken } = require("../utils/sendToken");
 
 exports.homepage = catchAsyncErrors(async (req, res, next) => {
@@ -42,4 +43,18 @@ exports.studentsignin = catchAsyncErrors(async (req, res, next) => {
 exports.studentsignout = catchAsyncErrors(async (req, res, next) => {
   res.clearCookie("token");
   res.json({message : "Successfully signout!"});
+});
+
+
+exports.studentsendmail = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findOne({email : req.body.email}).exec();
+  if(!student){
+    return next(
+      new ErrorHandler("User not found with this email address", 404)
+    );
+  }
+  const url = `${req.protocol}://${req.get("host")}/student/forget-link/${student.id}`
+
+  sendmail(req,res,next,url)
+  res.json({student, url});
 });
--- routes/indexRouters.js
+++ routes/indexRouters.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-
-const { homepage , studentsignup , studentsignin , studentsignout, currentUser} = require("../controllers/indexController");
+const {
+  homepage,
+  studentsignup,
+  studentsignin,
+  studentsignout,
+  currentUser,
+  studentsendmail,
+} = require("../controllers/indexController");
 const { isAutheticated } = require("../middlewares/auth");
 
+// GET /
 
-// GET / 
-
-router.get("/", isAutheticated , homepage )
+router.get("/", isAutheticated, homepage);
 
 // POST /stduent
-router.post("/student", isAutheticated , currentUser)
-
+router.post("/student", isAutheticated, currentUser);
 
 // POST  /student/signup
 router.post("/student/signup", studentsignup);
@@ -21,6 +25,9 @@ router.post("/student/signup", studentsignup);
 router.post("/student/signin", studentsignin);
 
 // GET  /student/signout
-router.get("/student/signout", isAutheticated , studentsignout);
+router.get("/student/signout", isAutheticated, studentsignout);
+
+// POST  /student/send-mail
+router.post("/student/send-mail", studentsendmail);
 
 module.exports = router;
